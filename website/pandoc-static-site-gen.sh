3f5dde43d0efd6808b2b9dfa4ee35002dabf4621
Erik Fastermann <erik@fastermann.de>
2019-01-18 15:50:02 +0100

    Added Pandoc static-site-generator

---
+++ website/pandoc-static-site-gen.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Minimalistic static-site-generator using Pandoc + Markdown.
+
+# Auto recompile and reload browser:
+# find . | entr -s "./pandoc-static-site-gen.sh && reload-browser Firefox"
+
+# Requirements:
+# https://pandoc.org/
+# http://eradman.com/entrproject/
+# https://github.com/trawor/XToDo
+# http://eradman.com/entrproject/scripts/reload-browser
+
+# Todo:
+# - auto-delete old files
+
+# Config:
+template_html="./.template/template.html"
+export_dir="./.site/"
+
+mkdir -p "$export_dir"
+
+for dir in $(find * -type d) ; do
+  mkdir -p "${export_dir}${dir}"
+done
+
+for file in $(find * -type f -name "*.md") ; do
+  pandoc --toc -t html5 --template="$template_html" "$file" -o "${export_dir}${file%.md}.html"
+done
+
+# Notes:
+# -not -path "*/\.*" -and -not -path ".*"
