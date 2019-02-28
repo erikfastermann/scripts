41cf7a5d2a271607e589ab0bc6179f5f85f5a3b0
Erik Fastermann <erik@fastermann.de>
2019-02-28 13:42:53 +0100

    Git-browse: Added preview, less, echo, checkout

--- git/browse.sh
+++ git/browse.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Browse commits for a specific file, view commit in $EDITOR
-# USAGE: browse.sh dir
+# Search through all commits or commits for a specific file
+# USAGE: browse.sh [file]
 
-git_folder="$1"
-if [[ "$git_folder" == "" ]]; then
-    git_folder="."
+set -e
+
+git_folder=$(git rev-parse --show-toplevel)
+
+git_file="$1"
+if ! [[ "$git_file" ]]; then
+    git_file=$(git -C "$git_folder" log --pretty=format: --name-only | sort -u | sed 's/^$/./' |
+        fzf --ansi --preview "git -C $git_folder log --color=always --all \
+        --format='%s %Cblue(%ar) %Cgreen<%an>%Creset' -- {}")
 fi
 
-while git_file="$(git -C $git_folder log --pretty=format: --name-only \
-    | sort -u | grep . | fzf)"; [[ "$git_file" ]]; do
-    while git_commit="$(git -C $git_folder log --follow --pretty=format:'%C(auto)%h %Cblue(%an %ar)%Creset %s' \
-        -- $git_file | fzf | cut -c1-7)"; [[ "$git_commit" ]]; do
-        git -C "$git_folder" show "${git_commit}:${git_file}" | "$EDITOR"
-    done
-done
+git -C "$git_folder" log --color=always --all --follow \
+    --format='%Cred%h %Creset%s %Cblue(%ar) %Cgreen<%an>%Creset' -- "$git_file" |
+    fzf --ansi --header "$git_file | Enter: Less | F1: Echo | F2: Editor | F3: Checkout" \
+    --preview "git -C $git_folder show --color=always \$(cut -c1-7 <<< {}) -- $git_file" \
+    --bind "enter:execute:git -C $git_folder show --color=always \$(cut -c1-7 <<< {}) -- $git_file | less -r" \
+    --bind "f1:execute:echo {}" \
+    --bind "f2:execute:git -C $git_folder show --color=always \$(cut -c1-7 <<< {}):${git_file} | $EDITOR" \
+    --bind "f3:execute:git -C $git_folder checkout \$(cut -c1-7 <<< {}) -- $git_file"
