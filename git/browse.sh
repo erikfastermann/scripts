1546fbd839799fc530dc7bdd3c854ad92e4f20b7
Erik Fastermann <erik@fastermann.de>
2019-02-28 16:19:24 +0100

    Git-browse: Copy hash to clipboard

--- git/browse.sh
+++ git/browse.sh
@@ -16,9 +16,10 @@ fi
 
 git -C "$git_folder" log --color=always --all --follow \
     --format='%Cred%h %Creset%s %Cblue(%ar) %Cgreen<%an>%Creset' -- "$git_file" |
-    fzf --ansi --header "$git_file | Enter: Less | F1: Echo | F2: Editor | F3: Checkout" \
-    --preview "git -C $git_folder show --color=always \$(cut -c1-7 <<< {}) -- $git_file" \
-    --bind "enter:execute:git -C $git_folder show --color=always \$(cut -c1-7 <<< {}) -- $git_file | less -r" \
+    fzf --ansi --header "$git_file | Enter: Less | F1: Echo | F2: Editor | F3: Yank hash | F4: Checkout" \
+    --preview "git -C $git_folder show --color=always \$(cut -d' ' -f1 <<< {}) -- $git_file" \
+    --bind "enter:execute:git -C $git_folder show --color=always \$(cut -d' ' -f1 <<< {}) -- $git_file | less -r" \
     --bind "f1:execute:echo {}" \
-    --bind "f2:execute:git -C $git_folder show --color=always \$(cut -c1-7 <<< {}):${git_file} | $EDITOR" \
-    --bind "f3:execute:git -C $git_folder checkout \$(cut -c1-7 <<< {}) -- $git_file"
+    --bind "f2:execute:git -C $git_folder show --color=always \$(cut -d' ' -f1 <<< {}):${git_file} | $EDITOR" \
+    --bind "f3:execute:echo {} | awk '{printf \$1}' | xclip" \
+    --bind "f4:execute:git -C $git_folder checkout \$(cut -d' ' -f1 <<< {}) -- $git_file"
