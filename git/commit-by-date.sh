c4d8df2af0b111a2464c9088bce716b1a41e99a1
Erik Fastermann <erik@fastermann.de>
2019-04-03 14:09:33 +0200

    Git-by-date: Uses stat, supports spaces in filenames

--- git/commit-by-date.sh
+++ git/commit-by-date.sh
@@ -3,8 +3,10 @@
 # Commit everything in the current folder by date.
 
 while read -r line; do
-    path=$(awk '{ print $NF }' <<< "$line")
-    date=$(awk '{ print $5 " " $6 " " $7 }' <<< "$line")
+    date=$(cut -d' ' -f1-3 <<< "$line")
+    path=$(cut -d' ' -f4- <<< "$line")
     git add "$path"
     git commit --date "$date" -m "Added ${path}."
-done < <(ls -lrt --full-time | tail -n +2)
+    #echo "$path"
+    #echo "$date"
+done < <(stat -c "%y %n" * | sort -n)
