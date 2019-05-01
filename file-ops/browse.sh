f24f6947741bdbae656f6b91aad37334b24020ee
Erik Fastermann <erik@fastermann.de>
2019-05-01 14:35:02 +0200

    File-ops: A file-browser using fzf

---
+++ file-ops/browse.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# A file-browser using fzf.
+# USAGE: . ./browse.sh
+
+file_menu='1 - Less
+2 - Editor'
+
+option_menu='9 - exit'
+
+while true; do
+    out="$(find . -maxdepth 1 -print0 | sed -z 's/^.$/../' |
+        fzf --read0 --ansi --header "$PWD" --preview \
+        'head -100 {} 2> /dev/null || ls --color=always -lhA {}' \
+        --print-query)"
+    exit_code="$?"
+    if [ "$exit_code" == '130' ]; then
+        break
+    fi
+    if [ "$exit_code" == '0' ]; then
+        file="$(sed '2q;d' <<< "$out")"
+        if [ -d "$file" ]; then
+            builtin cd "$file"
+            continue
+        fi
+        if [ -f "$file" ]; then
+            if menu="$(fzf --header "Option for $file" <<< "$file_menu")"; then
+                option="$(cut -d' ' -f1 <<< "$menu")"
+                case "$option" in
+                    1) less "$file" ;;
+                    2) "$EDITOR" "$file" ;;
+                esac
+            fi
+            continue
+        fi
+        echo 'Error!'
+        break
+    fi
+    if menu="$(fzf --header "Exit code: ${exit_code}. Select an option..." <<< "$option_menu")"; then
+        option="$(cut -d' ' -f1 <<< "$menu")"
+        case "$option" in
+            9) break ;;
+        esac
+    fi
+done
